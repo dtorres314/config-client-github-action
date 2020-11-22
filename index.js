f4a069c145e4f3197beea9e42ac79152c41a2e0e
trustme000777 <trustme000777@gmail.com>
2020-11-21 23:00:42 -0800

    README

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-/*
- * This action does a number of things for us:
- *
- * 1. It loads environment variables from Spring Cloud Config Server and loads them into the Github Actions workflow environment
- * 2. It also determines which environment we're in - `PRODUCTION` or `DEVELOPMENT` and exports that as an environment variable, `BP_MODE_LOWERCASE`.
- *
- * TODO
- *
- * 1. figure out how to poll the output of the kubectl command waiting for a config-server instance to appear somewhere
- *
- */
+
 
 function resolveConfiguration(configServerUsername,
                               configServerPassword,
@@ -41,29 +31,7 @@ function resolveConfiguration(configServerUsername,
         console.error(`stderr: ${stderr}`);
         return;
       }
-      //console.log(`stdout:\n${stdout}`);
-      // fs.copyFileSync(filename, process.env.GITHUB_ENV)
 
-      /*
-            fs.readFile(filename, 'utf8', (err, data) => {
-
-              if (err) {
-                return console.log(err)
-              }
-
-              const m = {}
-              const result = data.split(newline)
-              result.forEach((line, index, arr) => {
-                if (line.trim() === '' || line.indexOf('=') === -1) {
-                  return
-                }
-                const parts = line.split('=')
-                m[parts [0]] = parts[1]
-              })
-
-              callback(m)
-            });
-            */
     });
   });
 }
@@ -75,8 +43,6 @@ try {
 
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
-
-  // console.log(`The event payload: ${payload}`);
   const configServerUsername = core.getInput('config-server-username')
   const configServerPassword = core.getInput('config-server-password')
   const configServerAppId = core.getInput('config-server-application-id')
@@ -89,13 +55,13 @@ try {
     for (let prop in mapOfExportedVariables) {
       const value = mapOfExportedVariables[prop];
       core.exportVariable(prop, value)
-      // console.debug('exporting ' + prop);
     }
   }
 
   resolveConfiguration(configServerUsername, configServerPassword, configServerAppId, profile, configServerUri, callbackInWhichToProcessTheData)
 
-} catch (error) {
+} 
+catch (error) {
   core.setFailed(error.message);
 }
 

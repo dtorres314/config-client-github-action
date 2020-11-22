51ecd34184ce4d58f8fd703935366ba5e4053600
trustme000777 <trustme000777@gmail.com>
2020-11-21 19:59:06 -0800

    updating...

--- index.js
+++ index.js
@@ -9,95 +9,82 @@
  * 1. figure out how to poll the output of the kubectl command waiting for a config-server instance to appear somewhere
  *
  */
-const {exec} = require('child_process');
-const path = require('path')
-const tmp = require('tmp');
-const fs = require('fs');
+
+async function resolveConfiguration(configServerUsername,
+                                    configServerPassword,
+                                    configServerEnv,
+                                    bpMode,
+                                    configServerHost
+) {
+  const {exec} = require('child_process');
+  const path = require('path')
+  const tmp = require('tmp');
+  const fs = require('fs');
 
 
 // To get the filename
-console.log(`Filename is ${__filename}`);
+  console.log(`Filename is ${__filename}`);
 
 // To get the directory name
-console.log(`Directory name is ${__dirname}`);
-
-// todo resolve the binary for the config client based on which OS is running:
-//On Mac the variable returns darwin. On Windows, it returns win32 (even on 64 bit).
-//
-// Current possible values are:
-//
-// aix
-// darwin
-// freebsd
-// linux
-// openbsd
-// sunos
-// win32
-// I just set this at the top of my jakeFile:
-//
-// const isWin = process.platform === "win32";
-
-console.log('the platform is ' + process.platform)
-
-console.assert(process.platform === 'linux' || process.platform === 'darwin')
-
-const configCliPath = path.join(__dirname, 'bin', process.platform, 'config-client')
-
-console.log(`the path to the binary is ${configCliPath}`)
-
-const configServerUsername = process.env.CONFIGURATION_SERVER_USERNAME
-const configServerPassword = process.env.CONFIGURATION_SERVER_PASSWORD
-const configServerEnv = 'deployment'
-const bpMode = 'development'
-const configServerHost = 'http://35.193.213.137'
-const tmpObj = tmp.fileSync();
-
-console.log('File: ', tmpObj.name);
-console.log('File descriptor: ', tmpObj.fd);
-
-const filename = tmpObj.name
-const cmd = ` ${configCliPath} "${configServerUsername}" "${configServerPassword}" "${configServerEnv}" ${bpMode} ${configServerHost} ${filename}`.trim()
-
-console.log('the command is [' + cmd + ']');
+  console.log(`Directory name is ${__dirname}`);
 
 
-fs.readFile(filename, 'utf8', (err, data) => {
+  console.log('the platform is ' + process.platform)
 
-  if (err) return console.log(err)
+  console.assert(process.platform === 'linux' || process.platform === 'darwin')
+
+  const configCliPath = path.join(__dirname, 'bin', process.platform, 'config-client')
+
+  console.log(`the path to the binary is ${configCliPath}`)
+
+  const tmpObj = tmp.fileSync();
+
+  console.log('File: ', tmpObj.name);
+  console.log('File descriptor: ', tmpObj.fd);
+
+  const filename = tmpObj.name
+  const cmd = ` ${configCliPath} "${configServerUsername}" "${configServerPassword}" "${configServerEnv}" ${bpMode} ${configServerHost} ${filename}`.trim()
+
+  console.log('the command is [' + cmd + ']');
 
 
-  console.log('Before...')
-  console.log(`The length of the file data is ${data.length}`)
+  fs.readFile(filename, 'utf8', (err, data) => {
 
-  exec(cmd.trim(), (error, stdout, stderr) => {
+    if (err) return console.log(err)
 
 
-    if (error) {
-      console.error(`error: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.error(`stderr: ${stderr}`);
-      return;
-    }
-    console.log(`stdout:\n${stdout}`);
+    console.log('Before...')
+    console.log(`The length of the file data is ${data.length}`)
 
-    fs.readFile(filename, 'utf8', (err, data) => {
+    exec(cmd.trim(), (error, stdout, stderr) => {
 
-      if (err) {
-        return console.log(err)
+
+      if (error) {
+        console.error(`error: ${error.message}`);
+        return;
       }
-      console.log('After...')
-      console.log(`the length of the file data is ${data.length}`)
+      if (stderr) {
+        console.error(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(`stdout:\n${stdout}`);
+
+      fs.readFile(filename, 'utf8', (err, data) => {
+
+        if (err) {
+          return console.log(err)
+        }
+        console.log('After...')
+        console.log(`the length of the file data is ${data.length}`)
+
+      });
+
 
     });
-
-
   });
-});
 
 
-/*
+}
 
 const core = require('@actions/core');
 const github = require('@actions/github');
@@ -108,12 +95,22 @@ try {
   console.log(`Hello  ${nameToGreet} @ latest!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
-
-  // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+  const configServerUsername = process.env.CONFIGURATION_SERVER_USERNAME
+  const configServerPassword = process.env.CONFIGURATION_SERVER_PASSWORD
+  const configServerEnv = 'deployment'
+  const bpMode = 'development'
+  const configServerHost = 'http://34.71.92.231' // todo assign the configserver a DNS entry!
+  console.log(`going to connect to config server ${configServerHost} with user username ${configServerUsername}`)
+  const mapOfExportedVariables = await resolveConfiguration(configServerUsername, configServerPassword, configServerEnv, bpMode, configServerHost)
+  for (let prop in mapOfExportedVariables) {
+    const value = mapOfExportedVariables[prop];
+    core.exportVariable(prop, value)
+    console.log('exporting ' + prop + ' with a value ' + value.length + '.');
+  }
 }
 catch (error) {
   core.setFailed(error.message);
 }
-*/
+
